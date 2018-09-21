d4287697f71ee7e9ea0d95daece6908dc48d5e49
Limen2402 <Limen2402@gmail.com>
2018-09-21 18:42:58 +0300

    yop

--- main.c
+++ main.c
@@ -9,40 +9,15 @@
 #include <stddef.h>
 
 #define L 2000 //Lattice size
-#define PN 4000 //Particle number
-#define OT 3000000 //Observation time
 
 // структура треугольничек - точка и примыкающие к ней связи, правая и верхняя
 struct triangle{
-    double CV;  // characteristic time for vert
-    double CH;  //                     for hor
-	double AV;  // actual time for vert
-	double AH;  //             for hor
+    int flag;// were we here?
+
     int SV;  // state for vert
     int SH;  //       for hor
+
 };
-
-struct coord{
-    int X;
-    int Y;
-};
-
-//генерирует число, распределенное как t_0 * e^{ur} при r равномерно на [0,1]
-double gendouble(int t0, int u) 
-{
-    double r;
-    r = genrand64_real3();
-	return ((double)t0 * exp((double) u * r));
-} 
-
-//генерирует число, распределенное как ~ e^{-t\tau}
-double genlog(double tau)
-{
-    double r;
-    r = genrand64_real3();
-    return (double) (- tau * log(r));
-
-}
 
 //генерирует 1 с вер-тью p и 0 с вер-тью 1 - p
 int genSZ(double p) 
@@ -58,12 +33,60 @@ int genSZ(double p)
 
 
 struct triangle Lattice[L][L];
-struct coord Particles[PN];
+
+int corr_size;
+int corr_array[L / 2];
+
+
+// рекурсивный поиск в ширину
+int DFS(int X, int Y){
+
+    if (Lattice[X][Y].flag == 1)
+        return 0;
+	
+	Lattice[X][Y].flag = 1; // мы тут были
+	corr_size++;
+
+
+    if ((X < L - 2) && (Lattice[X][Y].SH == 1)) //можем ли мы пройти вправо?
+        DFS(X + 1, Y);
+
+    if ((Y < L - 2) && (Lattice[X][Y].SV == 1)) // вверх?
+        DFS(X, Y + 1);
+
+    if ((X > 0) && (Lattice[X - 1][Y].SH == 1)) // влево?
+        DFS(X - 1, Y);
+
+    if ((Y > 0) && (Lattice[X][Y - 1].SV == 1)) //вниз?
+        DFS(X, Y - 1);
+
+    return 0;
+
+}
+
+
+//Вычисляет заготовку для кор. функции и записывает ее во второй массив 
+//Так же поднимает флаги везде, где проходит - то есть, во всем кластере
+
+int corr_function(){
+
+    int i, center = L / 2;
+
+    DFS(center, center); //после этой рекурсивной команды, весь наш кластер будет иметь флаг 1
+
+    for(i = 0; i < L / 2; i++){
+        if(Lattice[center + i][center].flag == 1)   
+            corr_array[i]++;
+    }
+
+    return 0;
+
+}
 
 
 int main()
 {
-    FILE * histogramm_output;
+    FILE * corr_output1, * corr_output2;
 
 	//инициализация родного генератора временем
     srand((unsigned int) time (NULL)/2); 
@@ -73,211 +96,65 @@ int main()
 	unsigned long long init[4] = { rand(), rand(), rand(), rand() }, length = 4;
 	init_by_array64(init, length);	
 
-    int u, t0, i, j, k, t, Rmax, d, x, y, z, rSquare, center = L / 2, Mean, b, realization_number, Z, d1, d2;
+    int u, t0, i, j, k, t, Rmax, d, x, y, z, rSquare, center = L / 2, Mean, b, realization_number, Z;
     double p;
-	char s[40];
+	char s1[40], s2[40];
 
-	t0 = 100;
-	u = 120;
+	p = 0.490;
+	realization_number = 10000;
 
-    p = 0.500;
-    realization_number = 1;
+	sprintf(s1, "corr_size_L%dp%lf.txt", L, p); //вывод для n_s
+	sprintf(s2, "corr_lenght_L%dp%lf.txt", L, p); //вывод для расчета \xi
+	 
+    corr_output1 = fopen(s1, "a"); 
+	corr_output2 = fopen(s2, "a");
 
+
+    for(i = 1; i < L / 2; i++)
+        corr_array[i] = 0;
+	
     for(b = 0; b < realization_number; b++){
-
-        t = 0;
-        Rmax = 0;
 
         printf("%d\n", b);
 
         //initialize arrays
-        for(i = 0; i < L; i++)
-            for(j = 0; j < L; j++){
-                Lattice[i][j].CV = gendouble(t0, u);
-                Lattice[i][j].CH = gendouble(t0, u);
-                Lattice[i][j].AV = genlog(Lattice[i][j].CV);
-                Lattice[i][j].AH = genlog(Lattice[i][j].CH);
-                Lattice[i][j].SV = genSZ(p);
-                Lattice[i][j].SH = genSZ(p);
-            }
+		for (i = 0; i < L; i++) {
+			for (j = 0; j < L; j++) {
+				Lattice[i][j].flag = 0;
 
-        //place particles
-        for(k = 0; k < PN; k++){
-            Particles[k].Y = center;
-            Particles[k].X = center;
-        }
-
-
-        while((Rmax < center * center) && (t < OT)){
-            t++;
-            //Mean = 0;
-
-            for(k = 0; k < PN; k++){
-                //обрабатываем все частички
-                x = Particles[k].X;
-                y = Particles[k].Y;
-
-				//кусок кода, выбирающий направление прыжка
-				d1 = Lattice[x][y].SH + Lattice[x - 1][y].SH + Lattice[x][y].SV + Lattice[x][y - 1].SV;
-				if (d1 != 0) {
-					d2 = (int)floor(d1 * genrand64_real3());
-
-					d2 -= Lattice[x][y].SH;
-					if (d2 < 0) Particles[k].X++;
-					else {
-						d2 -= Lattice[x - 1][y].SH;
-						if (d2 < 0) Particles[k].X--;
-						else {
-							d2 -= Lattice[x][y].SV;
-							if (d2 < 0) Particles[k].Y++;
-							else Particles[k].Y--;
-						}
-					}
-				}
-
-				//проверяем чтобы частицы не вылетели за решетку
-                if (((Particles[k].X - center) * (Particles[k].X - center) + (Particles[k].Y - center) * (Particles[k].Y - center)) > Rmax * Rmax)
-					Rmax = (Particles[k].X - center) * (Particles[k].X - center) + (Particles[k].Y - center) * (Particles[k].Y - center);
-
-                //делаем нужные вычисления
-                //Mean += (Particles[k].X - center) * (Particles[k].X - center) + (Particles[k].Y - center) * (Particles[k].Y - center);
-
-            }
-
-			//прогресс
-			if (100 * t % OT == 0)
-				printf("%d%%\n", (int)(100 * t / OT));
-
-            for (i = 0; i < L; i++){
-                for(j = 0; j < L; j++){
-                    Lattice[i][j].AV -= 1;
-                    Lattice[i][j].AH -= 1;
-
-                    if (Lattice[i][j].AV < 1){
-                        Lattice[i][j].SV = 1 - Lattice[i][j].SV;
-                        Lattice[i][j].AV = genlog(Lattice[i][j].CV);
-                    }
-
-                    if (Lattice[i][j].AH < 1){
-                       Lattice[i][j].SH = 1 - Lattice[i][j].SH;
-                       Lattice[i][j].AH = genlog(Lattice[i][j].CH);
-                    }
-                }
-            }
-
-			//главный вывод
+				Lattice[i][j].SV = genSZ(p);
+				Lattice[i][j].SH = genSZ(p);
+			}
+		}
 			
-			if (t == 100) {
+		corr_size = 0;
 
-				sprintf(s, "P_L%dOT%d_u%dt0%d.txt", L, t, u, t0);
-				histogramm_output = fopen(s, "a");
-				for (i = 0; i < PN; i++)
-					fprintf(histogramm_output, "%lf ", sqrt((double)((Particles[i].X - center) * (Particles[i].X - center) + (Particles[i].Y - center) * (Particles[i].Y - center))));
-				fclose(histogramm_output);
+		corr_function();
 
-			}
-			
-			if (t == 300) {
-
-				sprintf(s, "P_L%dOT%d_u%dt0%d.txt", L, t, u, t0);
-				histogramm_output = fopen(s, "a");
-				for (i = 0; i < PN; i++)
-					fprintf(histogramm_output, "%lf ", sqrt((double)((Particles[i].X - center) * (Particles[i].X - center) + (Particles[i].Y - center) * (Particles[i].Y - center))));
-				fclose(histogramm_output);
-
-			}
-
-			if (t == 1000) {
-
-				sprintf(s, "P_L%dOT%d_u%dt0%d.txt", L, t, u, t0);
-				histogramm_output = fopen(s, "a");
-				for (i = 0; i < PN; i++)
-					fprintf(histogramm_output, "%lf ", sqrt((double)((Particles[i].X - center) * (Particles[i].X - center) + (Particles[i].Y - center) * (Particles[i].Y - center))));
-				fclose(histogramm_output);
-
-			}
-
-			if (t == 3000) {
-
-				sprintf(s, "P_L%dOT%d_u%dt0%d.txt", L, t, u, t0);
-				histogramm_output = fopen(s, "a");
-				for (i = 0; i < PN; i++)
-					fprintf(histogramm_output, "%lf ", sqrt((double)((Particles[i].X - center) * (Particles[i].X - center) + (Particles[i].Y - center) * (Particles[i].Y - center))));
-				fclose(histogramm_output);
-
-			}
-
-			if (t == 10000) {
-
-				sprintf(s, "P_L%dOT%d_u%dt0%d.txt", L, t, u, t0);
-				histogramm_output = fopen(s, "a");
-				for (i = 0; i < PN; i++)
-					fprintf(histogramm_output, "%lf ", sqrt((double)((Particles[i].X - center) * (Particles[i].X - center) + (Particles[i].Y - center) * (Particles[i].Y - center))));
-				fclose(histogramm_output);
-
-			}
-			
-			if (t == 30000) {
-
-				sprintf(s, "P_L%dOT%d_u%dt0%d.txt", L, t, u, t0);
-				histogramm_output = fopen(s, "a");
-				for (i = 0; i < PN; i++)
-					fprintf(histogramm_output, "%lf ", sqrt((double)((Particles[i].X - center) * (Particles[i].X - center) + (Particles[i].Y - center) * (Particles[i].Y - center))));
-				fclose(histogramm_output);
-
-			}
-
-			if (t == 100000) {
-
-				sprintf(s, "P_L%dOT%d_u%dt0%d.txt", L, t, u, t0);
-				histogramm_output = fopen(s, "a");
-				for (i = 0; i < PN; i++)
-					fprintf(histogramm_output, "%lf ", sqrt((double)((Particles[i].X - center) * (Particles[i].X - center) + (Particles[i].Y - center) * (Particles[i].Y - center))));
-				fclose(histogramm_output);
-
-			}
-
-			if (t == 300000) {
-
-				sprintf(s, "P_L%dOT%d_u%dt0%d.txt", L, t, u, t0);
-				histogramm_output = fopen(s, "a");
-				for (i = 0; i < PN; i++)
-					fprintf(histogramm_output, "%lf ", sqrt((double)((Particles[i].X - center) * (Particles[i].X - center) + (Particles[i].Y - center) * (Particles[i].Y - center))));
-				fclose(histogramm_output);
-
-			}
-
-			if (t == 1000000) {
-
-				sprintf(s, "P_L%dOT%d_u%dt0%d.txt", L, t, u, t0);
-				histogramm_output = fopen(s, "a");
-				for (i = 0; i < PN; i++)
-					fprintf(histogramm_output, "%lf ", sqrt((double)((Particles[i].X - center) * (Particles[i].X - center) + (Particles[i].Y - center) * (Particles[i].Y - center))));
-				fclose(histogramm_output);
-
-			}
-
-
-			if (t == 3000000) {
-
-				sprintf(s, "P_L%dOT%d_u%dt0%d.txt", L, t, u, t0);
-				histogramm_output = fopen(s, "a");
-				for (i = 0; i < PN; i++)
-					fprintf(histogramm_output, "%lf ", sqrt((double)((Particles[i].X - center) * (Particles[i].X - center) + (Particles[i].Y - center) * (Particles[i].Y - center))));
-				fclose(histogramm_output);
-
-			}
-			
-
-        }
-
-
+		fprintf(corr_output1, "%d ", corr_size);
     }
+	
 
-    printf("\7");
+    for(i = 0; i < L / 2; i++)
+        fprintf(corr_output2, "%d %d ", i, corr_array[i]);
+
+	fclose(corr_output1);
+	fclose(corr_output2);
+
+    printf("\7 \7 \7 \7 \7 \7 \7 \7 \7 \7");
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
